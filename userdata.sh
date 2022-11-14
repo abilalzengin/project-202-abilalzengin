165ffc6f3d7967d72deb55edf191bb4fcb769cb9
abilalzengin <bilalzengin@gmail.com>
2022-11-14 19:48:57 +0300

    changed

--- userdata.sh
+++ userdata.sh
@@ -6,7 +6,7 @@ pip3 install flask
 pip3 install flask_mysql
 cd /home/ec2-user && mkdir phonebook
 echo ${rds-endpoint} > /home/ec2-user/phonebook/dbserver.endpoint
-TOKEN="GHSAT0AAAAAABZ7LGITQ5XGSO46T6ERSOCIY3SNXVQ"
+TOKEN="add-token"
 FOLDER="https://$TOKEN@raw.githubusercontent.com/abilalzengin/project-202-abilalzengin/main"
 curl -s --create-dirs -o "/home/ec2-user/templates/index.html" -L "$FOLDER"/templates/index.html
 curl -s --create-dirs -o "/home/ec2-user/templates/add-update.html" -L "$FOLDER"/templates/add-update.html
